9563c3d009f035a4be7ec5459f44013e9804401e
MortyQ <yasenev1998@gmail.com>
2020-03-18 09:43:21 +0200

    Update

--- BackEnd/src/features/printing-editions/books-models.ts
+++ BackEnd/src/features/printing-editions/books-models.ts
@@ -1,9 +1,17 @@
 import {Document, Schema, Model, }  from 'mongoose';
 import mongoose from "mongoose";
 import { Print } from '../shared/enums/printing';
+import { ObjectId } from 'mongodb';
 
 
 export interface PrintongEdition extends mongoose.Document{
-
-    
+    name: string;
+    description: string;
+    cover_image: string;
+    removed_at: string;
+    type: Print;
+    price: number;
+    currency: string;
+    author_ids: ObjectId;
 }
+
